582e187e1b7618eaa6615cfdbdd09e767d28a39d
neuralvfx <neuralvfx@gmail.com>
2020-01-09 13:43:35 -0500

    cleaning notes

--- facial-pose-estimation-opencv/pose_estimate.h
+++ facial-pose-estimation-opencv/pose_estimate.h
@@ -9,20 +9,20 @@
 #include "opencv2/imgproc.hpp"
 #include "opencv2/dnn.hpp"
 #include "opencv2/dnn/shape_utils.hpp"
-#include "opencv2/opencv.hpp"
-#include "opencv2/imgproc.hpp"
 #include "dlib/image_processing.h"
 #include "dlib/opencv.h"
 #include "dlib/image_processing/frontal_face_detector.h"
 
 #include "pose_points.h"
+#include "utils.h"
+
 
 using namespace std;
 
 
-// Struct to pass data from DLL
 struct TransformData
 {
+	// Struct to pass data from DLL
 	TransformData(float tx, float ty, float tz, float rfx, float rfy, float rfz, float rux, float ruy, float ruz) :
 		tX(tx), tY(ty), tZ(tz), rfX(rfx), rfY(rfy), rfZ(rfz), ruX(rux), ruY(ruy), ruZ(ruz) {}
 	float tX, tY, tZ;
@@ -32,6 +32,7 @@ struct TransformData
 
 struct ExpressionData
 {
+	// Struct to pass facial blendhsapes
 	ExpressionData(float blendweight) :
 		blend_weight(blendweight) {}
 	float blend_weight;
@@ -42,7 +43,11 @@ class Estimator
 {
 public:
 
+	// Video feed
 	cv::VideoCapture _capture;
+
+	// Pose object
+	pose_points pose;
 
 	// Constant strings
 	const string caffe_config_file = "./deploy.prototxt";
@@ -50,15 +55,10 @@ public:
 	const string landmarks_model = "./shape_predictor_68_face_landmarks.dat";
 	const string expression_model = "./opt_model.onnx";
 
-	// Face box data
-	int face_width;
-	int center_x;
-	int center_y;
-
 	// Tick counter
 	int run_count;
 
-	// Capture Dimensions
+	// Capture dimensions
 	int frame_width;
 	int frame_height;
 	int scale_ratio;
@@ -78,22 +78,36 @@ public:
 	// Draw Axis on face
 	bool draw_points;
 
-	// Storage for reusable variables
-	cv::Point2f prev_nose;
-	vector< vector<cv::Point2f> > landmarks;
-	dlib::full_object_detection face_landmark;
-	cv::Mat expression_blob;
-	dlib::rectangle face_rect;
+	/*
+    Storage for reusable variables
+	*/
+
+	// Dlib landmark output
+	dlib::full_object_detection face_landmarks;
+	// Stream image storage
 	cv::Mat frame;
+	// Blenshshape output from Neural Net
 	cv::Mat expression;
+	// PnP landmakrs
 	vector <cv::Point3d> predicted_points_3d;
 	vector <cv::Point2d> landmark_points_2d;
+	// Output transforms for PnP solve
 	cv::Mat_<double> translation_vector;
 	cv::Mat_<double> rotation_vector;
+	// Camera initiation 
 	cv::Mat camera_matrix;
 	cv::Mat dist_coeffs;
+	// Face box data
+	cv::Point2f prev_nose;
+	dlib::rectangle face_rect;
+	int face_width;
+	int center_x;
+	int center_y;
 
-	// Storage for constant data arrays
+ 	/*
+	Storage for constant data arrays
+	*/
+
 	const vector<int> point_ids = { 6,7,8,9,10,17,18,19,20,21,22,23,24,25,26,27,28,29,30,31,32,
 		33,34,35,36,37,38,39,40,41,42,43,44,45,46,47,48,49,50,51,52,53,54,55,56,57,58,59,60,61,62,63,64,65,66,67 };
 
@@ -103,9 +117,6 @@ public:
 		cv::Scalar(256,256,128),cv::Scalar(128,0,256),cv::Scalar(128,128,128),cv::Scalar(128,0,128),cv::Scalar(128,256,0),cv::Scalar(256,256,256),cv::Scalar(256,128,0),
 		cv::Scalar(0,128,128),cv::Scalar(0,0,256),cv::Scalar(256,128,128),cv::Scalar(0,256,128),cv::Scalar(128,128,0),cv::Scalar(256,128,256),cv::Scalar(0,128,256),
 		cv::Scalar(128,256,256),cv::Scalar(256,0,0),cv::Scalar(256,256,0),cv::Scalar(0,128,0),cv::Scalar(0,256,0),cv::Scalar(256,0,128),cv::Scalar(128,256,128) };
-
-	// pose object
-	pose_points pose;
 
 
 public:
